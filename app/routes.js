9ae165f5683b9a7996dd16f83a60532c7dab205c
Ricky Nelson <rickyn@socketwiz.com>
2017-01-01 15:45:22 -0500

    Route generator mostly done

--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,6 @@ import React from 'react';
 import {Route} from 'react-router';
 import App from './modules/app';
 
-
 export default (
     <Route path="/" component={App}>
     </Route>
--- setup/generators/route/index.js
+++ setup/generators/route/index.js
@@ -2,9 +2,55 @@
  * Component Generator
  */
 
+const fs = require('fs');
+const componentExists = require('../utils/componentExists');
+
+function trimTemplateFile(template) {
+    // Loads the template file and trims the whitespace and then returns the content as a string.
+    return fs.readFileSync(`setup/generators/route/${template}`, 'utf8').replace(/\s*$/, '');
+}
+
 module.exports = {
     'description': 'Add a route',
-    'prompts': [],
-    'actions': []
+    'prompts': [{
+        'type': 'confirm',
+        'name': 'isContainer',
+        'default': true,
+        'message': 'Is this a container?'
+    }, {
+        'type': 'input',
+        'name': 'component',
+        'message': 'Which component should the route show?',
+        'validate': (value) => {
+            if ((/.+/).test(value)) {
+                return componentExists(value) ? true : `"${value}" doesn't exist.`;
+            }
+
+            return 'The path is required';
+        }
+    }, {
+        'type': 'input',
+        'name': 'path',
+        'message': 'Enter the path of the route.',
+        'default': '/about',
+        'validate': (value) => {
+            if ((/.+/).test(value)) {
+                return true;
+            }
+
+            return 'path is required';
+        }
+    }],
+    'actions': [{
+        'type': 'modify',
+        'path': '../../app/routes.js',
+        'pattern': /(import {Route} from 'react-router';\s*)/g,
+        'template': trimTemplateFile('route-import.hbs')
+    }, {
+        'type': 'modify',
+        'path': '../../app/routes.js',
+        'pattern': /({App}>\s*)/g,
+        'template': trimTemplateFile('route.hbs')
+    }]
 };
 
