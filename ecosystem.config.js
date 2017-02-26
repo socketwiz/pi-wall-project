bc432fe426d49d4e7613d3163664b03b4d7162b9
Ricky Nelson <rickyn@socketwiz.com>
2017-02-26 07:43:28 -0500

    Add a production deployment config

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+    // First application
+    {
+      name      : "pi-wall-project",
+      script    : "server/index.js",
+      env_production : {
+        NODE_ENV: "production",
+	PORT: 80
+      }
+    }
+  ]
+}
