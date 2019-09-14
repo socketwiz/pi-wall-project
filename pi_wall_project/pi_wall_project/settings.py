4142bbb543a96e7fbdadb0214c2b9526a269db62
Ricky Nelson <rickyn@socketwiz.com>
2019-09-13 21:30:51 -0400

    security updates

--- pi_wall_project/pi_wall_project/settings.py
+++ pi_wall_project/pi_wall_project/settings.py
@@ -129,4 +129,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
