dfc1a8357f1d31ba3bcd0a73d8a2c7bdc34c0c4e
Ricky Nelson <rickyn@socketwiz.com>
2019-11-26 07:54:51 -0500

    nginx/unicorn config

--- gunicorn/gunicorn-config.py
+++ gunicorn/gunicorn-config.py
@@ -1,2 +1,8 @@
 
-import multiprocessing bind = 'unix:/tmp/gunicorn.sock' workers = multiprocessing.cpu_count() * 2 + 1 reload = True daemon = True accesslog = './access.log' errorlog = './error.log'
+import multiprocessing
+bind = ':8000'
+workers = multiprocessing.cpu_count() * 2 + 1
+reload = True
+daemon = True
+accesslog = './access.log'
+errorlog = './error.log'
