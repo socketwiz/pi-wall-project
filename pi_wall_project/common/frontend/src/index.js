4656c576c8096171476f0d37bb81bdaeda010bae
Ricky Nelson <rickyn@socketwiz.com>
2020-04-05 14:00:26 -0400

    Remove more gunicorn
    * remove commented gunicorn
    * implement more websocket on frontend

--- pi_wall_project/common/frontend/src/index.js
+++ pi_wall_project/common/frontend/src/index.js
@@ -6,28 +6,23 @@
 const socket = new WebSocket(`ws://${window.location.host}/ws/common/`);
 
 socket.onmessage = (event) => {
-  console.log(event);
+  const data = event.data;
+  console.log(`ws: ${data}`);
+  switch (data) {
+    case 'redirect-photos':
+      window.location = '/';
+      break;
+    case 'redirect-weather':
+      window.location = '/weather';
+      break;
+    case 'redirect-wifi':
+      window.location = '/wifi';
+      break;
+    default:
+      console.error(`unknown-route: ${data}`);
+      break;
+  }
 };
 socket.onclose = (event) => {
   console.log(event);
 };
-// Listen for messages
-// socket.addEventListener('message', function eventListener(event) {
-//   const data = event.data;
-//   console.log(`ws: ${data}`);
-//   switch (data) {
-//     case 'redirect-photos':
-//       window.location = '/';
-//       break;
-//     case 'redirect-weather':
-//       window.location = '/weather';
-//       break;
-//     case 'redirect-wifi':
-//       window.location = '/wifi';
-//       break;
-//     default:
-//       console.error(`unknown-route: ${data}`);
-//       break;
-//   }
-// });
-console.log('common');
