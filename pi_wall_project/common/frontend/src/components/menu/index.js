ed7e01217a7b1aac1c190c7af752b32ca9b4ae43
Ricky Nelson <rickyn@socketwiz.com>
2020-04-06 12:57:26 -0400

    Fix button press

--- pi_wall_project/common/frontend/src/components/menu/index.js
+++ pi_wall_project/common/frontend/src/components/menu/index.js
@@ -29,6 +29,7 @@ class Menu extends Component {
     const socket = new WebSocket(`ws://${window.location.host}/ws/common/`);
 
     socket.onmessage = (event) => {
+      const {scalePosition} = this.state;
       const data = JSON.parse(event.data);
 
       if (typeof data.scale_position !== 'undefined') {
@@ -41,21 +42,24 @@ class Menu extends Component {
           this.setState({'menuOpen': false});
         }, 15 * SECOND);
       }
-      switch (data.scale_position) {
-        case 0:
-          window.location = '/';
-          break;
-        case 1:
-          window.location = '/weather';
-          break;
-        case 2:
-          window.location = '/wifi';
-          break;
-        default:
-          console.error(`unknown-route: ${data}`);
-          break;
-      }
+      if (typeof data.button_pressed !== 'undefined') {
+        switch (scalePosition) {
+          case 0:
+            window.location = '/';
+            break;
+          case 1:
+            window.location = '/weather';
+            break;
+          case 2:
+            window.location = '/wifi';
+            break;
+          default:
+            console.error(`unknown-route: ${data}`);
+            break;
+        }
+      };
     };
+
     socket.onclose = (event) => {
       console.log(event);
     };
